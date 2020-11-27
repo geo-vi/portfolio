eae88031bd37ab85bd2f16c505cf110fd3fccefe
George <ic3.sh0ck@gmail.com>
2020-11-27 22:41:29 +0100

    Stash

---
+++ src/containers/App/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div className={"App-main-container"}>
+            <h1 className={"App-logo"}>George Ivanov</h1>
+            <p className={"App-sub-header"}>geovi.dev</p>
+        </div>
+    </div>
+  );
+}
+
+export default App;

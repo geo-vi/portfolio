c8964819962ae5f204deb601752c6aa6fa59d96c
Geo-vi <me@geovi.dev>
2021-08-05 14:03:46 +0300

    * Changed the startscreen to a threejs label

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,7 +1,64 @@
-import React from "react";
+import React, {useMemo, useRef, useState, Suspense, useLayoutEffect} from "react";
 import './App.css';
 import { motion } from "framer-motion"
 import { FaGithub, FaLinkedin, FaEnvelope, FaMedium } from 'react-icons/fa';
+import {Canvas, useFrame, useLoader} from '@react-three/fiber'
+import * as THREE from 'three'
+
+function Label(props) {
+    const font = useLoader(THREE.FontLoader, process.env.PUBLIC_URL + '/droid_sans_regular.typeface.json')
+
+    const mesh = useRef();
+
+    useLayoutEffect(() => {
+        const size = new THREE.Vector3()
+        mesh.current.geometry.computeBoundingBox()
+        mesh.current.geometry.boundingBox.getSize(size)
+        mesh.current.position.x = -size.x / 2
+        mesh.current.position.y = -size.y / 2
+    }, [])
+
+    const config = useMemo(
+        () => ({ font, size: 2, height: 1 }),
+        [font]
+    )
+
+    useFrame(({ clock }) => (mesh.current.rotation.x = mesh.current.rotation.y = mesh.current.rotation.z = Math.sin(clock.getElapsedTime()) * 0.1))
+
+    return (
+            <mesh
+                {...props}
+                ref={mesh}
+            >
+                <textGeometry args={["Geovi's", config]} />
+                <meshNormalMaterial attach="material" color="red"/>
+            </mesh>
+    )
+}
+
+function Box(props) {
+    // This reference will give us direct access to the mesh
+    const mesh = useRef()
+    // Set up state for the hovered and active state
+    const [hovered, setHover] = useState(false)
+    const [active, setActive] = useState(false)
+    // Rotate mesh every frame, this is outside of React without overhead
+    useFrame(() => {
+        mesh.current.rotation.x = mesh.current.rotation.y += 0.01
+    })
+    return (
+        <mesh
+            {...props}
+            ref={mesh}
+            scale={active ? 1.5 : 1}
+            onClick={(e) => setActive(!active)}
+            onPointerOver={(e) => setHover(true)}
+            onPointerOut={(e) => setHover(false)}>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+        </mesh>
+    )
+}
 
 function App() {
     const buttonVariants = {
@@ -11,21 +68,18 @@ function App() {
 
     return (
         <div className="App">
-            <motion.div className={"App-main-container"}
-                        initial="hidden"
-                        animate="show">
-                <motion.h1 className={"App-logo"}
-                           initial={{y: -100, opacity: 0}}
-                           animate={{y: 0, opacity: 1}}
-                           transition={{
-                               type: "spring",
-                               stiffness: 260,
-                               damping: 20
-                           }}>George Ivanov
-                </motion.h1>
-                <motion.p className={"App-sub-header"}>geovi.dev</motion.p>
-                <motion.p className={"App-construction-msg"}>Portfolio is under construction & coming soon, stay tuned! 👷 🏗️</motion.p>
-            </motion.div>
+            <div className={"App-main-container"}>
+                <Canvas>
+                    <ambientLight intensity={0.5} />
+                    <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+                    <pointLight position={[-10, -10, -10]} />
+                    <Suspense fallback={null}>
+                        <Box position={[-1.2, 2, 0]} />
+                        <Box position={[1.2, 2, 0]} />
+                        <Label position={[-1, 0, 0]} />
+                    </Suspense>
+                </Canvas>
+            </div>
             <motion.div className={"App-main-footer"}>
                 <motion.a initial={"inactive"}
                           whileHover={"hovered"}
